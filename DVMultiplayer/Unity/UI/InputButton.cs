5f36e58ed68626f0c213ee475c5a4ce0849bddc6
Halm, Patrick P.M. van <pvanhalm00@gmail.com>
2021-01-29 22:43:39 +0100

    Add input scripts

---
+++ DVMultiplayer/Unity/UI/InputButton.cs
@@ -0,0 +1,31 @@
+﻿using DVMultiplayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+class InputButton : MonoBehaviour
+{
+    public char key;
+    public bool isBackspace;
+    Button button;
+    InputScreen input;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        input = transform.parent.GetComponent<InputScreen>();
+
+        button.onClick.AddListener(() =>
+        {
+            if (!isBackspace)
+                input.SendKeyPress(key);
+            else
+                input.Backspace();
+        });
+    }
+}
---
+++ DVMultiplayer/Unity/UI/InputScreen.cs
@@ -0,0 +1,52 @@
+﻿using DVMultiplayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+class InputScreen : MonoBehaviour
+{
+    private TextMeshProUGUI label;
+
+    public string Input { get; internal set; }
+
+    private void Awake()
+    {
+        label = transform.Find("Label Input").GetComponent<TextMeshProUGUI>();
+        ResetInput();
+    }
+     
+    public void ResetInput()
+    {
+        Input = "";
+        UpdateLabel();
+    }
+
+    public void SendKeyPress(char key)
+    {
+        Input += key;
+        UpdateLabel();
+    }
+
+    private void UpdateLabel()
+    {
+        label.text = Input;
+    }
+
+    public void Backspace()
+    {
+        if (Input.Length <= 0)
+            return;
+        
+        Input = Input.Substring(0, Input.Length - 1);
+        UpdateLabel();
+    }
+
+    public void SetTitle(string inputWindowTitle)
+    {
+        transform.Find("Title").GetComponent<TextMeshProUGUI>().text = $"Entering {inputWindowTitle}";
+    }
+}
