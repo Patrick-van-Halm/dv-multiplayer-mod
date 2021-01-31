6abd8438a32ad38fef310308493322fb15229c77
Halm, Patrick P.M. van <pvanhalm00@gmail.com>
2021-01-31 10:45:03 +0100

    Add FindAll children of transform function

--- DVMultiplayer/Utils/UGameObject.cs
+++ DVMultiplayer/Utils/UGameObject.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
 
 namespace DVMultiplayer.Utils
 {
@@ -7,6 +9,30 @@ namespace DVMultiplayer.Utils
         public static bool Exists(string name)
         {
             return GameObject.Find(name) != null;
+        }
+
+        public static IEnumerable<Transform> FindAll(this Transform transform, string name)
+        {
+            List<Transform> transforms = new List<Transform>();
+            for(int i = 0; i < transform.childCount; i++)
+            {
+                Transform child = transform.GetChild(i);
+                if(Regex.IsMatch(child.gameObject.name, name))
+                {
+                    transforms.Add(child);
+                }
+            }
+            return transforms;
+        }
+
+        public static IEnumerable<T> GetComponent<T>(this IEnumerable<Transform> transforms)
+        {
+            List<T> components = new List<T>();
+            foreach (Transform transform in transforms)
+            {
+                components.Add(transform.GetComponent<T>());
+            }
+            return components;
         }
     }
 }
