97de8e8bb4dd7fb08a3533a3d118b4fbc9631988
Patrick-van-Halm <pvanhalm00@gmail.com>
2021-02-19 15:16:21 +0100

    Allow clients to generate jobs too

--- DVMultiplayer/Patches/StopGeneratingJobsAsClient.cs
+++ DVMultiplayer/Patches/StopGeneratingJobsAsClient.cs
@@ -1,18 +1,54 @@
-﻿using DVMultiplayer.Networking;
+﻿using DV.Logic.Job;
+using DVMultiplayer.Networking;
 using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace DVMultiplayer.Patches
 {
 #pragma warning disable IDE0060 // Remove unused parameter
 #pragma warning disable IDE0051 // Remove unused private members
-    [HarmonyPatch(typeof(StationProceduralJobsController), "TryToGenerateJobs")]
+    [HarmonyPatch(typeof(StationController), "Update")]
     internal class StopGeneratingJobsAsClient
     {
-        private static bool Prefix()
+        private static bool Prefix(StationController __instance, StationJobGenerationRange ___stationRange, Station ___logicStation, ref bool ___attemptJobOverviewGeneration, ref bool ___playerEnteredJobGenerationZone, ref HashSet<Job> ___processedNewJobs, ref List<JobOverview> ___spawnedJobOverviews, PointOnPlane ___jobBookletSpawnSurface)
         {
-            if (NetworkManager.IsClient() && !NetworkManager.IsHost())
-                return false;
-
+            if (NetworkManager.IsClient())
+            {
+				if (___logicStation == null || !SaveLoadController.carsAndJobsLoadingFinished)
+				{
+					return false;
+				}
+				if (___stationRange.IsPlayerInRangeForBookletGeneration(___stationRange.PlayerSqrDistanceFromStationOffice) && ___attemptJobOverviewGeneration)
+				{
+					for (int i = 0; i < ___logicStation.availableJobs.Count; i++)
+					{
+						Job job = ___logicStation.availableJobs[i];
+						if (!___processedNewJobs.Contains(job))
+						{
+							PointOnPlane pointOnPlane = ___jobBookletSpawnSurface;
+							ValueTuple<Vector3, Quaternion> valueTuple = (pointOnPlane != null) ? pointOnPlane.GetRandomPointWithRotationOnPlane() : new ValueTuple<Vector3, Quaternion>(__instance.transform.position, __instance.transform.rotation);
+							Transform parent = SingletonBehaviour<WorldMover>.Exists ? SingletonBehaviour<WorldMover>.Instance.originShiftParent : null;
+							JobOverview item = BookletCreator.CreateJobOverview(job, valueTuple.Item1, valueTuple.Item2, parent);
+							___spawnedJobOverviews.Add(item);
+							___processedNewJobs.Add(job);
+						}
+					}
+					___attemptJobOverviewGeneration = false;
+				}
+				float playerSqrDistanceFromStationCenter = ___stationRange.PlayerSqrDistanceFromStationCenter;
+				bool isPlayerInGenerationZone = ___stationRange.IsPlayerInJobGenerationZone(playerSqrDistanceFromStationCenter);
+				if (isPlayerInGenerationZone && !___playerEnteredJobGenerationZone)
+				{
+					if(!SingletonBehaviour<NetworkPlayerManager>.Instance.GetPlayers().All(p => ___stationRange.IsPlayerInJobGenerationZone((p.transform.position - ___stationRange.stationCenterAnchor.position).sqrMagnitude)))
+						__instance.ProceduralJobsController.TryToGenerateJobs();
+					___playerEnteredJobGenerationZone = true;
+					return false;
+				}
+				return false;
+            }
             return true;
         }
     }
