ac176dcb80d6437ab7aaea46e1961a7c14030455
Patrick-van-Halm <pvanhalm00@gmail.com>
2021-03-01 20:01:31 +0100

    Fix NRE UpdateWheelslip

---
+++ DVMultiplayer/Patches/Train/DrivingForcePatch.cs
@@ -0,0 +1,21 @@
+﻿using DV;
+using DV.Logic.Job;
+using DVMultiplayer.Networking;
+using HarmonyLib;
+using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace DVMultiplayer.Patches
+{
+#pragma warning disable IDE0060 // Remove unused parameter
+#pragma warning disable IDE0051 // Remove unused private members
+    [HarmonyPatch(typeof(DrivingForce), "UpdateWheelslip")]
+    internal class DrivingForcePatch
+    {
+        private static bool Prefix(float inputForce, Bogie bogie, float maxTractionForcePossible)
+        {
+            return bogie && bogie.Car;
+		}
+    }
+}
